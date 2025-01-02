64aaf82bfeb6ff0a3977ad9eede84a5971082adc
Deepak Katariya <deepak.sorthiya@gmail.com>
2025-01-03 00:17:24 +0530

    integrated angular client in maven

---
+++ src/main/java/com/example/config/SpaWebFilter.java
@@ -0,0 +1,36 @@
+package com.example.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SpaWebFilter extends OncePerRequestFilter {
+
+    /**
+     * Forwards any unmapped paths (except those containing a period) to the client {@code index.html}.
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        // Request URI includes the contextPath if any, removed it.
+        String path = request.getRequestURI().substring(request.getContextPath().length());
+        if (
+            !path.startsWith("/api") &&
+            !path.startsWith("/server-info") &&
+            !path.startsWith("/actuator") &&
+            !path.startsWith("/v3/api-docs") &&
+            !path.startsWith("/h2-console") &&
+            !path.contains(".") &&
+            path.matches("/(.*)")
+        ) {
+            request.getRequestDispatcher("/index.html").forward(request, response);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
